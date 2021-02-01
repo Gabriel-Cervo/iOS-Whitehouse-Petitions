285896efa57ab3832bd645e1eae3354678ec5bcb
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-01 08:44:43 -0300

    ADD leitura de arquivo JSON e info na tableView

---
+++ 100DaysOfSwift-Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  100DaysOfSwift-Project7
+//
+//  Created by João Gabriel Dourado Cervo on 01/02/21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ 100DaysOfSwift-Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  100DaysOfSwift-Project7
+//
+//  Created by João Gabriel Dourado Cervo on 01/02/21.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
