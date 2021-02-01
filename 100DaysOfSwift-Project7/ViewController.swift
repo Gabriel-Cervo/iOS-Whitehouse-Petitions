b9497a30d519e3fe6d178c7b484379a5899da9db
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-02-01 08:17:11 -0300

    ADD metodos padroes de table view no viewController

--- 100DaysOfSwift-Project7/ViewController.swift
+++ 100DaysOfSwift-Project7/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Title"
+        cell.detailTextLabel?.text = "Details"
+        
+        return cell
     }
 
 
